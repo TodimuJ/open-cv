874a379b81146a825771a9c5eeae0d228de07d94
Todimu Jenrola <todimujenrola@gmail.com>
2020-11-17 21:44:21 -0500

    Commit

--- chapter1.py
+++ chapter1.py
@@ -16,7 +16,7 @@ def editImage():
     cv2.waitKey(0)
 
 def useVideo():
-    cv2.imshow("Output", img )
+    #cv2.imshow("Output", img )
 
     cap = cv2.VideoCapture("./video.mp4")
 
@@ -86,25 +86,43 @@ def warpPerspective():
     #print(img.shape)
     cv2.waitKey(0)
 
+def detectColor():
 
-def empty(a):
-    pass
-
-img = cv2.imread("./lambo.jpeg")
-cv2.namedWindow("TrackBars")
-cv2.resizeWindow("TrackBars", 640, 240)
-cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty )
-cv2.createTrackbar("Hue Max", "TrackBars", 179, 179, empty )
-cv2.createTrackbar("Sat Min", "TrackBars", 0, 255, empty )
-cv2.createTrackbar("Sat Max", "TrackBars", 255, 255, empty )
-cv2.createTrackbar("Val Min", "TrackBars", 0, 255, empty )
-cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty )
+    def empty(a):
+        pass
 
 
-imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-h_min = cv2.getTrackbarPos()
+    cv2.namedWindow("TrackBars")
+    cv2.resizeWindow("TrackBars", 640, 240)
+    cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty )
+    cv2.createTrackbar("Hue Max", "TrackBars", 179, 179, empty )
+    cv2.createTrackbar("Sat Min", "TrackBars", 0, 255, empty )
+    cv2.createTrackbar("Sat Max", "TrackBars", 255, 255, empty )
+    cv2.createTrackbar("Val Min", "TrackBars", 0, 255, empty )
+    cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty )
+
+    while True:
+        img = cv2.imread("./lambo.jpeg")
+        imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+        h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+        s_min = cv2.getTrackbarPos("Sat  Min", "TrackBars")
+        s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+        v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+        v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+        #print(v_min)
+        # lower1 = np.array([h_min, s_min, v_min])
+        # upper1 = np.array([h_max, s_max, v_max])
+        lower = np.array([000, 000, 186])
+        upper = np.array([179, 255, 255])
+        mask = cv2.inRange(imgHSV, lower, upper)
+        imgResult = cv2.bitwise_and(img, img, mask=mask )
+
+        cv2.imshow("Original", img)
+        cv2.imshow("HSV", imgHSV)
+        cv2.imshow("Mask", mask)
+        cv2.imshow("Result", imgResult)
+        cv2.waitKey(0)
 
 
-cv2.imshow("Original", img)
-cv2.imshow("HSV", imgHSV)
-cv2.waitKey(0)
+detectColor()
