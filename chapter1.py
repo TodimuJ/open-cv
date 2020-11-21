6f1b82be615da7ba6dc03a3e30bc12e491a2b64f
Todimu Jenrola <todimujenrola@gmail.com>
2020-11-21 15:34:25 -0500

    Commit

--- chapter1.py
+++ chapter1.py
@@ -126,20 +126,38 @@ def detectColor():
 
 def morph():
     img = cv2.imread('./shapes.png')
+    imgContour = img.copy()
     
+    def contour(img):
+        contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            print(area)
+
+            if area > 10:
+                cv2.drawContours(imgContour, cnt, -1, (255,0,0), 3)
+                peri = cv2.arcLength(cnt, True)
+                print(peri)
+                approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+                print(len(approx))
+                objCor = len(approx)
+                x,y,w,h = cv2.boundingRect(approx)
+
+                cv2.rectangle(imgContour, (x,y), (x+w,y+h), (0, 255, 0), 2)
+
 
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (7,7), 1)
     imgCanny = cv2.Canny(imgBlur,50,50)
 
+    contour(imgCanny)
 
     cv2.imshow("Original", img)
     cv2.imshow("Gaussian", imgBlur)
     cv2.imshow("Canny", imgCanny)
+    cv2.imshow("Contour", imgContour)
     cv2.waitKey(0)
-
-def contours(img):
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL)
 
 
 
