a00ccd27a555942243bfd9f2ecf48694eaaa9509
Todimu Jenrola <todimujenrola@gmail.com>
2020-11-22 21:23:52 -0500

    Commit

--- chapter1.py
+++ chapter1.py
@@ -175,6 +175,20 @@ def morph():
     cv2.imshow("Contour", imgContour)
     cv2.waitKey(0)
 
+def faces():
+
+    faceCascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
+    img = cv2.imread('./lena.png')
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+
+    cv2.imshow("Result", img)
+    cv2.waitKey(0)
 
 
-morph()
+
+faces()
