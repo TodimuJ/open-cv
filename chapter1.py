0e514c3e131229532bf402b90418ccf2d2aa77dd
Todimu Jenrola <todimujenrola@gmail.com>
2020-11-25 18:51:22 -0500

    Commit

--- chapter1.py
+++ chapter1.py
@@ -124,6 +124,57 @@ def detectColor():
         cv2.imshow("Result", imgResult)
         cv2.waitKey(0)
 
+def findColor(): #Finds the color of an object by user varying slider to mask out object
+
+    def empty(a):
+        pass
+
+    frameWidth = 640
+    frameHeight = 480
+
+    cap = cv2.VideoCapture(0)
+    cap.set(3, frameWidth)
+    cap.set(4, frameHeight)
+    cap.set(10, 150)
+
+    cv2.namedWindow("HSV")
+    cv2.resizeWindow("HSV", 640, 240)
+    cv2.createTrackbar("Hue Min", "HSV", 0, 179, empty )
+    cv2.createTrackbar("Sat Min", "HSV", 0, 255, empty )
+    cv2.createTrackbar("Val Min", "HSV", 0, 255, empty )
+    cv2.createTrackbar("Hue Max", "HSV", 179, 179, empty )
+    cv2.createTrackbar("Sat Max", "HSV", 255, 255, empty )
+    cv2.createTrackbar("Val Max", "HSV", 255, 255, empty )
+
+    while True:
+        _, img = cap.read()
+        imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+        h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+        s_min = cv2.getTrackbarPos("Sat  Min", "TrackBars")
+        s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+        v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+        v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+        #print(v_min)
+        lower = np.array([h_min, s_min, v_min])
+        upper = np.array([h_max, s_max, v_max])
+        mask = cv2.inRange(imgHSV, lower, upper)
+        result = cv2.bitwise_and(img, img, mask=mask )
+
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+        cv2.imshow("Original", img)
+        cv2.imshow("Mask", mask)
+        cv2.imshow("Result", result)
+
+        if cv2.waitKey(1) & 11111111 == ord('q'):
+            break
+
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 def morph():
     img = cv2.imread('./shapes.png')
     imgContour = img.copy()
@@ -218,4 +269,4 @@ def marker():
 
 
 
-marker()
+findColor()
